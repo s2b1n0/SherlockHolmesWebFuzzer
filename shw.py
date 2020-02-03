9e61898a03a69405fee468119034e04a0b9639f6
João Sabino <joao.nsabino@gmail.com>
2020-02-03 17:36:15 -0300

    functions deployed

--- shw.py
+++ shw.py
@@ -10,29 +10,32 @@ else:
 if argExist == True:
     url = sys.argv[1]
 '''
-host = 'https://google.com/'
+host = 'http://localhost/'
 filesFounds = []
 bkpOldFiles = ['.bkp', '.bak', '.src', '.dev', '.txt', '.old', '.inc', '.orig', '.copy', '.tmp']
 
-with open("/var/www/html/common.txt.bkp") as file:
-    archive = file.readlines()
-    for i in archive:
-
-        page = i.replace(" ","")
-        page = page.replace("\n", "")
-
-        url = host+page
-        r = requests.get(url)
+def bocf(url):
+    for i in bkpOldFiles:
+        url2 = url + i
+        r = requests.get(url2)
         r = r.status_code
-
         if r == 200:
-            print("File found:", url)
+            return print("BOC  found:", url2,'Must be interessanting')
 
-            for i in bkpOldFiles:
-                url2 = url + i
-                r = requests.get(url2)
-                r = r.status_code
+def main():
+    with open("/var/www/html/common.txt.bkp") as file:
+        archive = file.readlines()
+        for i in archive:
 
-                if r == 200:
-                    print("File found:", url2)
+            page = i.replace(" ","")
+            page = page.replace("\n", "")
 
+            url = host+page
+            r = requests.get(url)
+            r = r.status_code
+
+            if r == 200:
+                print("File found:", url)
+                bocf(url)
+
+main()
